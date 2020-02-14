5ed9d186bb22c23e3ba01520449c8fe5a9c9c008
Sutolix <tg884ght@gmail.com>
2020-02-14 12:24:34 -0300

    estrutura redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import Routes from './routes';
 import './index.css';
 import Header from './components/Header';
 
 export default function App() {
- return (
-   <BrowserRouter>
-   <Header />
-    <Routes />
-   </BrowserRouter>
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
+import './home.css'
+import api from '../../services/api';
 
 export default function Home() {
+
+  const [trips, setTrips] = useState([]);
+  
+  useEffect(() => {
+    async function loadAPI(){
+      const response = await api.get('trips');
+      setTrips(response.data);
+    }
+
+    loadAPI();
+  }, []);
+
  return (
    <div>
-       <h1>Home</h1>
+       <div className="box">
+         {trips.map(trip => (
+           <li key={trip.id}>
+             <img src={trip.image} alt={trip.title}/>
+             <strong>{trip.title}</strong>
+             <span> Status: {trip.status ? 'Disponivel' : 'Indisponivel'} </span>
+
+             <button
+              type="button"
+              onClick={() => {}}
+              >
+                <div>
+                  <MdFlightTakeoff size={16} color="#ccc" />
+                </div>
+                <span>SOLICITAR RESERVA</span>
+              </button>
+           </li>
+         ))}
+       </div>
    </div>
  );
 }
