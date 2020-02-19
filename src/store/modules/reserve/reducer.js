6be9a5b3613bbe9a9990c30da8d0b56c02c71e17
Sutolix <tg884ght@gmail.com>
2020-02-19 10:59:18 -0300

    ponto de salvamento antes de refatorar

--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -21,6 +21,25 @@ export default function reserve(state = [], action) {
                     });
                 }
             });
+
+        case 'REMOVE_RESERVE':
+            return produce(state, draft => {
+                //Pega o id que será excluído baseado em que foi clicado
+                const tripIndex = draft.findIndex(trip => trip.id === action.id);
+                
+
+                //Caso haja mais de uma reserva para a mesma trip, excluirá uma por uma
+                if (draft[tripIndex].amount >= 2){
+                    draft[tripIndex].amount -=1;
+                    return
+                }
+
+                if(tripIndex >= 0 ){
+                    //draft.splice exclui o dado (tripIndex na posição 1 já que é o único pois não há repetição de id)
+                    draft.splice(tripIndex, 1);
+                }
+            
+            });
         default:
             return state;
     }
