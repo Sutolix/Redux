a9ebd0b677094251973710399ab4a8da84b7b2f6
Sutolix <tg884ght@gmail.com>
2020-02-27 09:27:30 -0300

    segunda refatorada

--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -5,26 +5,9 @@ export default function reserve(state = [], action) {
         case 'ADD_RESERVE_SUCCESS':
             //state original e draft que será a copia dela onde faremos as modificações
             return produce(state, draft => {
-
-                //Compara se já existe uma reserva com o mesmo id da qual está sendo adicionad
-                //Caso houver, a resposta para essa constante será maior que 1
-                const tripIndex = draft.findIndex(trip => trip.id === action.trip.id)
-
-                if(action.trip.status === false){
-                    alert("Essa viagem está indisponivel no momento");
-                    return
-                }
-
-                if(tripIndex >= 0){
-                    //ele(amount) igual a ele mesmo mais um
-                    draft[tripIndex].amount +=1;
-                }else{
-                    //adiciona a trip
-                    draft.push({
-                        ...action.trip,
-                        amount: 1,
-                    });
-                }
+                //adiciona a trip ao array
+                draft.push(action.trip);
+                
             });
 
         case 'REMOVE_RESERVE':
@@ -58,3 +41,28 @@ export default function reserve(state = [], action) {
             return state;
     }
 }
+
+
+/*OLD CODE ADD_RESERVE_SUCESS*/
+/*
+//Compara se já existe uma reserva com o mesmo id da qual está sendo adicionad
+    //Caso houver, a resposta para essa constante será maior que 1
+    const tripIndex = draft.findIndex(trip => trip.id === action.trip.id)
+
+    if(action.trip.status === false){
+        alert("Essa viagem está indisponivel no momento");
+        return
+    }
+
+    if(tripIndex >= 0){
+    //ele(amount) igual a ele mesmo mais um
+        draft[tripIndex].amount +=1;
+    }else{
+    //adiciona a trip
+        draft.push({
+            ...action.trip,
+            amount: 1,
+        });
+    
+    }
+*/
--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,13 +1,32 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { addReserveSuccess, updateAmountReserve } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({id}){
-    const response = yield call(api.get, `trips/${id}`)
-
-    yield put(addReserveSuccess(response.data))
+    /*Verifica se a trip existe retornando para a váriavel*/
+    const tripExiste = yield select(
+        state => state.reserve.find(trip => trip.id === id)
+    );
+    /*Define o que fazer dependendo da existência da trip*/
+    if(tripExiste){
+        //aumenta o amount
+        const amount = tripExiste.amount + 1;
+        //atualiza o valor do amount
+        yield put(updateAmountReserve(id, amount));
+    }else{
+        //pega os dados da trip conforme seu id
+        const response = yield call(api.get, `trips/${id}`)   
+        //define o amount junto do response.data
+        const data = {
+            ...response.data,
+            amount: 1,
+        };
+        //dispara a função que adiciona a reserva passando a const data
+        yield put(addReserveSuccess(data))
+    }
 }
 
 export default all([
+    //caso o usuário clique várias vezes seguidas no botão somente a última vez será considerada
     takeLatest('ADD_RESERVE_REQUEST', addToReserve)
 ])
